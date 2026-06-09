6ede7e17dcf52a7162280734da54ff10e3ab8145
Priyanshu Dhakre <priyanshudhakre26@gmail.com>
2026-06-09 22:01:35 +0530

    [Priyanshu]add distance in yards and miles Question

---
+++ gcr-codebase/programming-elements/level2/DistanceinYardsandMiles.java
@@ -0,0 +1,17 @@
+package level2;
+
+import java.util.Scanner;
+
+public class DistanceinYardsandMiles {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double distanceInFeet = input.nextDouble();
+
+        double yards = distanceInFeet / 3;
+        double miles = yards / 1760;
+
+        System.out.println("The distance in yards is "
+                + yards + " while the distance in miles is " + miles);
+    }
+}
