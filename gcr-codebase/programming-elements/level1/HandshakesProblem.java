54d6588b3ff060c48552f4d65521b1b7625ca61c
Priyanshu Dhakre <priyanshudhakre26@gmail.com>
2026-06-09 21:51:44 +0530

    [Priyanshu]add handshakes problem Question

---
+++ gcr-codebase/programming-elements/level1/HandshakesProblem.java
@@ -0,0 +1,15 @@
+package Level1;
+
+import java.util.Scanner;
+
+public class HandshakesProblem {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        int handshakes = (n * (n - 1)) / 2;
+
+        System.out.println("Maximum number of handshakes is " + handshakes);
+    }
+}
