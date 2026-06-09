b2a5cfa4a89feb5288377de9da1adc8acafc07dd
Priyanshu Dhakre <priyanshudhakre26@gmail.com>
2026-06-09 21:54:33 +0530

    [Priyanshu]add side of square Question

---
+++ gcr-codebase/programming-elements/level1/SideofSquare.java
@@ -0,0 +1,15 @@
+package Level1;
+
+import java.util.Scanner;
+
+public class SideofSquare {
+     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double perimeter = input.nextDouble();
+        double side = perimeter / 4;
+
+        System.out.println("The length of the side is " + side +
+                " whose perimeter is " + perimeter);
+    }
+}
