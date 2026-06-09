f50366e3e559d4e57e63863f5b4f581ffc299033
Priyanshu Dhakre <priyanshudhakre26@gmail.com>
2026-06-09 21:50:50 +0530

    [Priyanshu]add feet to yards Question

---
+++ gcr-codebase/programming-elements/level1/FeettoYards_Miles.java
@@ -0,0 +1,17 @@
+package Level1;
+
+import java.util.Scanner;
+
+public class FeettoYards_Miles {
+     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double feet = input.nextDouble();
+
+        double yards = feet / 3;
+        double miles = yards / 1760;
+
+        System.out.println("Distance in yards is " + yards +
+                " and in miles is " + miles);
+    }
+}
