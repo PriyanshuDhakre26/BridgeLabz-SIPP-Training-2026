648b3c46a730c24e4eb66acb5190587f9e696ccc
Priyanshu Dhakre <priyanshudhakre26@gmail.com>
2026-06-14 18:41:48 +0530

    [Priyanshu]add table multiplication code

---
+++ gcr-codebase/control-flow/Array/level-1/MultiplicationTable.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+
+    static void generateTable(int num) {
+        int[] table = new int[10];
+
+        for (int i = 1; i <= 10; i++) {
+            table[i - 1] = num * i;
+        }
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " * " + i + " = " + table[i - 1]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        generateTable(num);
+    }
+}
