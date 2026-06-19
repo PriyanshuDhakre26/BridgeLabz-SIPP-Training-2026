cfbc1c8607f49acd21d0c20d55ce13f38b42055d
Priyanshu Dhakre <priyanshudhakre26@gmail.com>
2026-06-19 20:11:04 +0530

    [priyanshu]add palindrome string problem code

---
+++ gcr-codebase/programming-elements/Secenario_based_questions/palindromeString.java
@@ -0,0 +1,70 @@
+package Secenario_based_questions;
+// A spy agency encodes messages. Reverse a secret message, check if
+// it is a palindrome, count vowels/consonants, and verify if two
+// intercepts are anagrams. Extend: find the first non-repeating
+// character in a surveillance log.
+import java.util.*;
+public class palindromeString {
+   public static void main(String[] args) {
+        String str="madan";
+        String reversed="";
+       
+        for(int i=str.length()-1;i>=0;i--){
+            reversed+=str.charAt(i);
+        }
+       
+        if(str.equals(reversed)){
+            System.out.println(str + " is a palindrome.");
+        } else {
+            System.out.println(str + " is not a palindrome.");
+        }
+      
+        int vowels=0;
+        int consonants=0;
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
+                vowels++; 
+            }
+            else if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z')){
+                consonants++; 
+            }
+   } 
+   System.out.println("Vowels: " + vowels);
+   System.out.println("Consonants: " + consonants);
+   
+   String str1="listen";
+   String str2="silent";
+   if(str1.length()!=str2.length()){
+    System.out.println(str1 + " and " + str2 + " are not anagrams.");   
+   }
+    else{
+     char[] arr1=str1.toCharArray();
+     char[] arr2=str2.toCharArray();
+     Arrays.sort(arr1);
+     Arrays.sort(arr2);
+     if(Arrays.equals(arr1, arr2)){
+          System.out.println(str1 + " and " + str2 + " are anagrams.");
+     }
+     else{
+          System.out.println(str1 + " and " + str2 + " are not anagrams.");
+     }
+    }
+    
+    String log="swiss";
+    char nonRepeating=' ';
+    for(int i=0;i<log.length();i++){
+        char ch=log.charAt(i);
+        if(log.indexOf(ch)==log.lastIndexOf(ch)){
+            nonRepeating=ch;
+            break;
+        }
+    }
+    if(nonRepeating!=' '){
+        System.out.println("First non-repeating character: " + nonRepeating);
+    }
+    else{
+        System.out.println("No non-repeating character found.");
+    }
+}
+}
