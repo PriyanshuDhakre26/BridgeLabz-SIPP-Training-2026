61db865d1085127677cb9da0088fa286ef55fa36
Priyanshu Dhakre <priyanshudhakre26@gmail.com>
2026-07-05 17:34:34 +0530

    [Priyanshu]add area and perimeter calculation of shapes  using abstraction and encapsulation

---
+++ gcr-oops_codebase/EncapsulationAndAbstraction/ScenarioQuestion/Shape_Hierarchy.java
@@ -0,0 +1,96 @@
+abstract class Shape {
+
+    public abstract double area();
+
+    public abstract double perimeter();
+}
+
+class Circle extends Shape {
+
+    private final double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+class Rectangle extends Shape {
+
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double area() {
+        return length * width;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * (length + width);
+    }
+}
+
+class Triangle extends Shape {
+
+    private double a;
+    private double b;
+    private double c;
+
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    @Override
+    public double area() {
+        double s = (a + b + c) / 2;
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+
+    @Override
+    public double perimeter() {
+        return a + b + c;
+    }
+}
+
+public class Shape_Hierarchy {
+
+    public static void main(String[] args) {
+
+        Shape[] shapes = {
+            new Circle(5),
+            new Rectangle(4, 6),
+            new Triangle(3, 4, 5)
+        };
+
+        System.out.println("Shape Report");
+        System.out.println("----------------------------");
+
+        for (Shape shape : shapes) {
+            System.out.println("Shape: " + shape.getClass().getSimpleName());
+            System.out.printf("Area: %.2f%n", shape.area());
+            System.out.printf("Perimeter: %.2f%n", shape.perimeter());
+            System.out.println();
+        }
+    }
+}
